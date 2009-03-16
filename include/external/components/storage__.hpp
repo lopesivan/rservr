7611cc8f4db1dfb108a48ea19c96c846a107c212
developer <developer@localhost>
2009-03-16 21:30:33 +0000

    gamma.2 - updated clist

--- include/external/components/storage__.hpp
+++ include/external/components/storage__.hpp
@@ -1,34 +1,30 @@
-/* This software is released under the BSD License.
- |
- | Copyright (c) 2009, Kevin P. Barry
- | All rights reserved.
- |
- | Redistribution  and  use  in  source  and   binary  forms,  with  or  without
- | modification, are permitted provided that the following conditions are met:
- |
- | - Redistributions of source code must retain the above copyright notice, this
- |   list of conditions and the following disclaimer.
- |
- | - Redistributions in binary  form must reproduce the  above copyright notice,
- |   this list  of conditions and the following disclaimer in  the documentation
- |   and/or other materials provided with the distribution.
- |
- | - Neither the  name  of  the  Resourcerver  Project  nor  the  names  of  its
- |   contributors may be  used to endorse or promote products  derived from this
- |   software without specific prior written permission.
- |
- | THIS SOFTWARE IS  PROVIDED BY THE COPYRIGHT HOLDERS AND  CONTRIBUTORS "AS IS"
- | AND ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING, BUT  NOT LIMITED TO, THE
- | IMPLIED WARRANTIES OF  MERCHANTABILITY  AND FITNESS FOR A  PARTICULAR PURPOSE
- | ARE DISCLAIMED.  IN  NO EVENT SHALL  THE COPYRIGHT  OWNER  OR CONTRIBUTORS BE
- | LIABLE  FOR  ANY  DIRECT,   INDIRECT,  INCIDENTAL,   SPECIAL,  EXEMPLARY,  OR
- | CONSEQUENTIAL   DAMAGES  (INCLUDING,  BUT  NOT  LIMITED  TO,  PROCUREMENT  OF
- | SUBSTITUTE GOODS OR SERVICES;  LOSS  OF USE,  DATA,  OR PROFITS;  OR BUSINESS
- | INTERRUPTION)  HOWEVER  CAUSED  AND ON  ANY  THEORY OF LIABILITY,  WHETHER IN
- | CONTRACT,  STRICT  LIABILITY, OR  TORT (INCLUDING  NEGLIGENCE  OR  OTHERWISE)
- | ARISING IN ANY  WAY OUT OF  THE USE OF THIS SOFTWARE, EVEN  IF ADVISED OF THE
- | POSSIBILITY OF SUCH DAMAGE.
- +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*============================================================================*\
+ *CList list library. Internal storage types.                                 *
+ *Copyright (C) 2009 Kevin P. Barry                                           *
+ *                                                                            *
+ *This library is free software; you can redistribute it and/or modify it     *
+ *under the terms of the GNU Lesser General Public License as published by the*
+ *Free Software Foundation; either version 2.1 of the License, or (at your    *
+ *option) any later version.                                                  *
+ *                                                                            *
+ *This library is distributed in the hope that it will be useful, but WITHOUT *
+ *ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ *FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+ *for more details.                                                           *
+ *                                                                            *
+ *You should have received a copy of the GNU Lesser General Public License    *
+ *along with this library; if not, write to the                               *
+ *                                                                            *
+ *    Free Software Foundation, Inc.,                                         *
+ *    59 Temple Place, Suite 330,                                             *
+ *    Boston, MA 02111-1307 USA                                               *
+ *                                                                            *
+ *Please contact ta0kira@users.sourceforge.net for feedback, questions, or    *
+ *comments.                                                                   *
+\*============================================================================*/
+
+//CList_________________________________________________________________________
+//(C) 2007,2009 Kevin P. Barry
 
 //Header Section################################################################
 #ifndef storage___HPP
@@ -219,7 +215,7 @@ template <class Type> class internal_storage <Type, true>
     if (!this->actual_value(pPos)) return true;
     Type *Hold = this->actual_value(pPos);
     this->actual_value(pPos) = NULL;
-    if (!is_a_slave)
+    if (!is_a_slave && Hold)
      {
     allocator.destroy(Hold);
     allocator.deallocate(Hold, 1);
@@ -415,7 +411,7 @@ template <class Type> class internal_storage <Type, true>
     if (pPos > virt_storage.size()) return;
     Type *Hold = *(virt_storage.begin() + pPos);
     *(virt_storage.begin() + pPos) = NULL;
-    if (!is_a_slave)
+    if (!is_a_slave && Hold)
      {
     allocator.destroy(Hold);
     allocator.deallocate(Hold, 1);
