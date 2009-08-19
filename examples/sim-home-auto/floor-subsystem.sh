075cd3a5e1187d860f109a99a5a75a688b5765b3
developer <developer@localhost>
2009-08-19 06:38:54 +0000

    gamma.3 - updated sim-home-auto example

--- examples/sim-home-auto/floor-subsystem.sh
+++ examples/sim-home-auto/floor-subsystem.sh
@@ -3,23 +3,22 @@
 # This script creates a server system for each virtual floor with one client for
 # each room on the floor.
 
+server="$1"
+shift
+
 { echo "execute_critical @rservrd@-dxr";
-  echo "execute @rsv-fsrelay@$1-connect";
+  echo "execute @rsv-fsrelay@$server-connect";
   echo "register_all_wait"; } | \
-rservr "$1" /dev/null
+rservr "$server" /dev/null
 
 for room in $*; do
-  if [ "$room" = "$1" ]; then
-    continue
-  fi
-
   name=$( echo "$room" | cut -d: -f1 )
   lights=$( echo "$room" | cut -d: -f2 )
   fans=$( echo "$room" | cut -d: -f3 )
   vents=$( echo "$room" | cut -d: -f4 )
 
-  rservrd "$1" @execute@separate-room@$name@$lights@$fans@$vents > /dev/null
+  rservrd "$server" @execute@separate-room@$name@$lights@$fans@$vents > /dev/null
 done
 
 #connect the virtual floor's server system to the main system
-rservrd "$1" @local_connect@"$1"-connect@/tmp/system-connect > /dev/null
+rservrd "$server" @local_connect@"$1"-connect@/tmp/system-connect > /dev/null
