1bb7724d5c148a8043d0b6f6c7275790df0aa0ca
developer <developer@localhost>
2010-07-23 20:03:05 +0000

    gamma.6 - updated build chain and added rservrd macro inference from param.h

--- project-stats.sh
+++ project-stats.sh
@@ -12,14 +12,14 @@ if [ "$1" = "all" ];then
 fi
 
 if [ "$1" = "all" ];then
-  additional_really="../hparser/{include,src}"
-  additional_really_specific="echo ../hparser/Makefile.am; "
+  additional_really="../../hparser/trunk/{include,src}"
+  additional_really_specific="echo ../../hparser/trunk/Makefile.am; "
 fi
 
 patterns="$additional_really include/{rservr,global$additional} lang libs plugins programs security -name \"*.h\" -o -name \"*.hpp\" -o -name \"*.c\" -o -name \"*.cpp\" -o -name Makefile.am"
 any_line="\".*\""
 nonblank_line="."
-find_command="( find $patterns -type f; echo param.h; echo attributes.h; $additional_really_specific)"
+find_command="( find $patterns -type f; echo param.h; echo attributes.h; $additional_really_specific )"
 
 echo $find_command
 echo
@@ -29,7 +29,7 @@ echo 'FILES:   ' $(eval $find_command | grep -c .)
 echo 'LINES:   ' $(cat `eval $find_command` | eval grep -c $any_line) '(all)'
 
 #'uncomment' is local because I wrote it!
-echo 'RELEVANT:' $(cat `eval $find_command` | uncomment | grep -c .) '(non-blank/non-comment)'
+echo 'RELEVANT:' $(cat `eval $find_command` | ./uncomment | grep -c .) '(non-blank/non-comment)'
 
 echo 'SIZE:    ' $(du -chs `eval $find_command` | tail -n1)
 
@@ -51,7 +51,7 @@ echo
 echo 'HIGHEST 8 UNUSED LINES: (non-API / minus license header)'
 eval $find_command | grep -v "include/rservr/" | while read file; do
   total=$( sed -r 'btest; {:remove;N;/\*\//{N;d;b};bremove}; :test;1,/\*\// {/[ \t]*\/\*/bremove}' "$file" | grep -c ".*" )
-  used=$( uncomment "$file" | grep -c "." )
+  used=$( ./uncomment "$file" | grep -c "." )
 
   if [ "$total" -ne 0 ]; then
     ratio=$( echo "100*($total-$used)/($total)" | bc | sed -r '{:start;s/^([0-9]{,2})(\.|$)/0\1\2/g;tstart}' )
@@ -65,8 +65,8 @@ done | \
 echo
 echo 'HIGHEST 8 AVERAGE LINE LENGTH: (non-Makefile / non-blank/non-comment)'
 eval $find_command | grep -v Makefile.am | while read file; do
-  used=$( uncomment "$file" | grep -c "." )
-  characters=$( uncomment "$file" | sed -r '{:a;s/([^\n])([^\n]+)$/\1\n\2/g;ta}' | grep -c . )
+  used=$( ./uncomment "$file" | grep -c "." )
+  characters=$( ./uncomment "$file" | sed -r '{:a;s/([^\n])([^\n]+)$/\1\n\2/g;ta}' | grep -c . )
 
   if [ "$used" -ne 0 ]; then
     ratio=$( echo "scale=1;$characters/$used" | bc | sed -r '{:start;s/^([0-9]{,3})(\.|$)/0\1\2/g;tstart}' )
